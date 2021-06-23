e5ae85abbcdac7b231e8946e11dff03dac5705a2
John <John@DESKTOP-JS7BL0G>
2021-06-23 18:10:31 -0400

    First Commit

---
+++ DrinkBuilder/src/DrinkBuilder.java
@@ -0,0 +1,13 @@
+
+public class DrinkBuilder
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Blah");
+		for (int i = 0; i<10;i++)
+		{
+			System.out.println("Blah" + i);
+		}
+	}
+
+}
