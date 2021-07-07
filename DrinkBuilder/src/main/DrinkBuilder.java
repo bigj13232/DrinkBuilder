3d8d742359c2524e15682a34a455684011ebe1c6
John <John@DESKTOP-JS7BL0G>
2021-07-07 18:43:05 -0400

    Update DrinkBuilder

--- DrinkBuilder/src/main/DrinkBuilder.java
+++ DrinkBuilder/src/main/DrinkBuilder.java
@@ -1,10 +1,45 @@
 package main;
 
+import java.util.Scanner;
+
+import drink.DrinkHelper;
+import helpers.InputHelper;
+import helpers.MenuHelper;
+
 public class DrinkBuilder
 {
 	public static void main(String[] args)
-	{
+	{		
+		/**
+		 * Class variables
+		 */
+		int menuOption = 1;
+		Scanner menuInput = new Scanner(System.in);
 		
+		do
+		{
+			MenuHelper.displayMenu(MenuHelper.MAIN_MENU);
+			System.out.print("Please enter selection: ");
+			menuOption = InputHelper.getInt(menuInput);
+			
+			
+			switch(menuOption) {
+				case 1:
+					DrinkHelper.addDrink();
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				default:
+					System.out.println("Invalid menu option, try again");
+					break;
+			}
+		}while(menuOption != 0);
+		
+		menuInput.close();
 	}
 
 }
